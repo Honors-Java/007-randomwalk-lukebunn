aafdb3a3386498afef0c21f4f4ace9aad6aee71f
lukebunn1 <superpantsftw1@gmail.com>
2020-11-21 04:29:40 +0000

    I kept getting unknown script errors

--- index.java
+++ index.java
@@ -16,17 +16,36 @@ void setup() {
 void draw() {
 	stroke(1);
   //starts random walking from 200,200
-  rect(x, y, 10, 10);
+ rect(x, y, 10, 10);
   
+
+
   //Here is where you should create the new rectangles to create the illusion of 'random walking'
   // You will need to make sure you dont draw more rectangles than the limit of steps
   //You'll need to use the Math.random() to decide how to create randomness in your drawing of rectangles.
-  // Keep your canvas and rectangle width and height as I've given you
+  // Keep your canvas and rectangle width and height as I've given you  
+int steps = (int)(Math.random() * 4);
+    if(steps==0){
+      fill(255);
+      rect(x,y,10,10);
+      x = x + 10;
+    }else if(Steps==1){
+      fill(255);
+      rect(x,y,10,10);
+      x = x + 10;
+    }else if(steps==2){
+      fill(255);
+      rect(x,y,10,10);
+      y = y + 10;
+    }else if(steps==3){
+      fill(255);
+     rect(x,y,10,10);
+     y = y - 10;
+    }
 
- 
-  
-    println("drew rect at: " + x + ", " + y);
-    println("Steps taken: " + steps);
+
+  println("drew rect at: " + x + ", " + y);
+  println("Steps taken: " + steps);
 }
 
 
